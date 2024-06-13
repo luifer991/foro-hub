7880d419ed98b6fc52fe23d75bb590dbd9f06ff9
Jorge Morales <luifer991@gmail.com>
2024-06-12 22:57:11 -0500

    creating the service manager for ths post topic and the queries for the database

---
+++ src/main/java/com/foro/foro_hub/domain/topic/RegistroTopicoService.java
@@ -0,0 +1,45 @@
+package com.foro.foro_hub.domain.topic;
+
+import com.foro.foro_hub.domain.curso.CursoRepository;
+import com.foro.foro_hub.domain.usuario.UsuarioRepository;
+import com.foro.foro_hub.infra.errors.ValidationExist;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class RegistroTopicoService {
+    @Autowired
+    private TopicoRepository topicoRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private CursoRepository cursoRepository;
+    
+    public DatosRespuestaTopico registrar ( DatosRegistroTopico datos ) {
+        
+        if ( ! cursoRepository.findById( datos.idUsuario() ).isPresent() ) {
+            throw new ValidationExist( "El usuario no existe" );
+        }
+        
+        if ( topicoRepository.findByTitulo( datos.titulo() ).isPresent() ) {
+            throw new ValidationExist( "El titulo ya existe" );
+        }
+        
+        if ( topicoRepository.findByMensaje( datos.mensaje() ).isPresent() ) {
+            throw new ValidationExist( "El mensaje ya existe" );
+        }
+        
+        var usuario = usuarioRepository.findById( datos.idUsuario() ).get();
+        var curso = cursoRepository.findById( datos.idCurso() ).get();
+        var fecha = LocalDateTime.now();
+        var status = Status.NUEVO;
+        
+        var topico = new Topico ( null, datos.titulo(), datos.mensaje(), fecha, status, usuario, curso );
+        
+        topicoRepository.save ( topico );
+        
+        return new DatosRespuestaTopico ( topico );
+    }
+}
--- src/main/java/com/foro/foro_hub/domain/topic/TopicoRepository.java
+++ src/main/java/com/foro/foro_hub/domain/topic/TopicoRepository.java
@@ -1,6 +1,16 @@
 package com.foro.foro_hub.domain.topic;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+     Optional<Topico> findByTitulo ( String titulo );
+     
+     Optional<Topico> findByMensaje ( String mensaje );
+     
+     Page<Topico> findAllByFechaTrue ( Pageable paginacion );
 }
+
