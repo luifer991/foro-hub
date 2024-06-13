1fd47093e2308b87f848920e965cde7ee08f064a
Jorge Morales <luifer991@gmail.com>
2024-06-13 16:11:38 -0500

    topico controller completed

--- src/main/java/com/foro/foro_hub/controller/TopicoController.java
+++ src/main/java/com/foro/foro_hub/controller/TopicoController.java
@@ -14,24 +14,47 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping ( "/topicos" )
 public class TopicoController {
-
+    
     @Autowired
     private RegistroTopicoService registroTopicoService;
     @Autowired
+    private ActualizarTopicoService actualizarTopicoService;
+    @Autowired
     private TopicoRepository topicoRepository;
-
+    
     @PostMapping
     @Transactional
     public ResponseEntity nuevoTopico (
-            @RequestBody @Valid DatosRegistroTopico datosRegistroTopico) {
+            @RequestBody @Valid DatosRegistroTopico datosRegistroTopico ) {
         var response = registroTopicoService.registrar( datosRegistroTopico );
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok( response );
     }
     
     @GetMapping
-    public ResponseEntity<Page<DatosListadoTopico>> listarTopicos (
-            @PageableDefault(size = 10, sort = { "fecha" }, direction = Sort.Direction.ASC ) Pageable paginacion ) {
+    public ResponseEntity <Page <DatosListadoTopico>> listarTopicos (
+            @PageableDefault ( size = 10, sort = { "fecha" }, direction = Sort.Direction.ASC ) Pageable paginacion ) {
         var page = topicoRepository.findAllByFechaTrue( paginacion ).map( DatosListadoTopico::new );
         return ResponseEntity.ok( page );
+    }
+    
+    @GetMapping ( "/{id}" )
+    public ResponseEntity detallar ( @PathVariable Long id ) {
+        var topico = topicoRepository.getReferenceById( id );
+        return ResponseEntity.ok( new DatosListadoTopico( topico ) );
+    }
+    
+    @PutMapping ( "/{id}" )
+    @Transactional
+    public ResponseEntity actualizar ( @PathVariable Long id,
+                                       @RequestBody @Valid DatosActualizarTopico datosActualizarTopico ) {
+        var response = actualizarTopicoService.actualizar( id, datosActualizarTopico );
+        return ResponseEntity.ok( response );
+    }
+    
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity eliminar ( @PathVariable Long id ) {
+        topicoRepository.deleteById( id );
+        return ResponseEntity.noContent().build();
     }
 }
