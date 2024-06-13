64bd7551dfeb1b9fb4156b62ee579b64d1de8b8b
Jorge Morales <luifer991@gmail.com>
2024-06-12 22:53:58 -0500

    remove old constructor

--- src/main/java/com/foro/foro_hub/domain/topic/Topico.java
+++ src/main/java/com/foro/foro_hub/domain/topic/Topico.java
@@ -12,36 +12,27 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "topicos")
+@Table ( name = "topicos" )
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode (of = "id")
+@EqualsAndHashCode ( of = "id" )
 public class Topico {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue ( strategy = GenerationType.IDENTITY )
     private Long id;
     private String titulo;
     private String mensaje;
     private LocalDateTime fecha;
-    @Enumerated(EnumType.STRING)
+    @Enumerated ( EnumType.STRING )
     private Status status;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "usuario_id")
+    @ManyToOne ( fetch = FetchType.LAZY )
+    @JoinColumn ( name = "usuario_id" )
     private Usuario usuario;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "curso_id")
+    @ManyToOne ( fetch = FetchType.LAZY )
+    @JoinColumn ( name = "curso_id" )
     private Curso curso;
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "respuesta_id")
-    private Respuesta respuestas;
-    
-    public Topico ( DatosRegistroTopico datosRegistroTopico) {
-        this.titulo = datosRegistroTopico.titulo();
-        this.mensaje = datosRegistroTopico.mensaje();
-        this.fecha = LocalDateTime.now();
-        this.status = Status.NUEVO;
-        this.usuario = new Usuario(usuario.getLogin());
-        this.curso = new Curso(curso.getNombre());
-    }
+//    @ManyToOne ( fetch = FetchType.LAZY )
+//    @JoinColumn ( name = "respuesta_id" )
+//    private Respuesta respuestas;
 }
