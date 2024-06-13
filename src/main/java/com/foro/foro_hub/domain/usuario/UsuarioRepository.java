b6bbe37b9f764cd2b4cc9ebc3dc2b02b78a6fa9f
Jorge Morales <luifer991@gmail.com>
2024-06-12 22:59:13 -0500

    comment the security configurations

--- src/main/java/com/foro/foro_hub/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/foro/foro_hub/domain/usuario/UsuarioRepository.java
@@ -3,10 +3,11 @@ package com.foro.foro_hub.domain.usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+//import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
     @Query ( "SELECT u FROM Usuario u WHERE u.login = :username" )
-    UserDetails findByLoginUSer ( @Param ( "username" ) String username );
-    
+    List<Usuario> findByLoginUser ( @Param ( "username" ) String username ); //UserDetails
 }
