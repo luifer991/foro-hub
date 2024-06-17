3307499218b1cf2e659831aafa601b4fad92b896
Jorge Morales <luifer991@gmail.com>
2024-06-17 14:17:37 -0500

    create perfil repository

---
+++ src/main/java/com/foro/foro_hub/domain/profile/PerfilRepository.java
@@ -0,0 +1,6 @@
+package com.foro.foro_hub.domain.profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerfilRepository extends JpaRepository<Perfil, Long>{
+}
